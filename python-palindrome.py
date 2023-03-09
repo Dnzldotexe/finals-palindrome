88fea956eb13990a1bfaa49a353988298e54e375
Dnzl.exe <cempronziel55@gmail.com>
2023-03-09 15:20:37 +0800

    added a pseudocode

---
+++ python-palindrome.py
@@ -0,0 +1,15 @@
+# PSEUDOCODE
+
+# START
+# DISPLAY 'Palindrome Checker'
+# DISPLAY 'Please input a Word:'
+# x is equal to INPUT
+# y is equal to REVERSE of x
+# IF x is equal to y 
+#     DISPLAY 'The input is Palindrome'
+# ELSE 
+#     DISPLAY 'The input isn't a Palindrome'
+# END
+
+# SOURCE CODE
+
