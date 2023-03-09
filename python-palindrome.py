592ecbe4883855b2169a002cc265107da9d9ddae
Dnzl.exe <cempronziel55@gmail.com>
2023-03-09 15:43:18 +0800

    coded the palindrome

--- python-palindrome.py
+++ python-palindrome.py
@@ -2,7 +2,7 @@
 
 # START
 # DISPLAY 'Palindrome Checker'
-# DISPLAY 'Please input a Word:'
+# DISPLAY 'Please String:'
 # x is equal to INPUT
 # y is equal to REVERSE of x
 # IF x is equal to y 
@@ -11,5 +11,15 @@
 #     DISPLAY 'The input isn't a Palindrome'
 # END
 
-# SOURCE CODE
 
+# SOURCE CODE
+print('\n')
+print("Palindrome Checker")
+x = str(input("Enter String: "))
+
+# compares the x to the reversed x
+# [::-1] returns string in reverse
+if x == x[::-1]:
+    print(f"'{x}' is a palindrome")
+else:
+    print(f"'{x}' isn't a palindrome")
